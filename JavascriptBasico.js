c74920fa6446b3132c7d9594ca44e6ed58811ef9
Lucas Gauna <lks.gauna@gmail.com>
2022-11-27 19:50:29 -0300

    first commit

---
+++ JavascriptBasico.js
@@ -0,0 +1,146 @@
+//Guia ejercicios FreeCodeCamp
+
+//Ejercicio 1 - Comentarios en JS 
+
+// Comentario de linea  
+
+/* Comentario de bloque 
+ o multilinea */
+
+//Ejercicio 2 - Declaracion de variables
+
+var myName ; 
+
+//Ejercicio 3 - Asignacion de variables
+
+var a;
+a = 7;
+
+//Ejercicio 4 - Declaracion de variables con asignacion
+
+var a = 9;
+
+//Ejercicio 5 - Declaracion de variables string
+
+var myFirstName = "Lucas";
+var myLastName = "Gauna";
+
+//Ejercicio 6 - Understanding Uninitialized Variables
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//Ejercicio 7 - Understanding Case Sensitivity in Variables
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//Ejercicio 8 - Explore Differences Between the var and let Keywords
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//Ejercicio 8 - Declare a Read-Only Variable with the const Keyword
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+//Ejercicio 9 - Add Two Numbers with JavaScript
+
+const sum = 10 + 10;
+
+//Ejercicio 10 - Subtract One Number from Another with JavaScript
+
+const difference = 45 - 33;
+
+//Ejercicio 11 - Multiply Two Numbers with JavaScript
+
+const product = 8 * 10;
+
+//Ejercicio 12 - Divide One Number by Another with JavaScript
+
+const quotient = 66 / 33;
+
+//Ejercicio 13 - Increment a Number with JavaScript
+
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+//Ejercicio 14 - Decrement a Number with JavaScript
+
+let myVar1 = 11;
+
+// Only change code below this line
+myVar1--;
+
+//Ejercicio 15 - Create Decimal Numbers with JavaScript
+
+var ourDecimal = 5.7;
+
+// Only change code below this line
+var myDecimal = 5.7;
+
+//Ejercicio 16 - Multiply Two Decimals with JavaScript
+
+var product1 = 2.0 * 2.5;
+
+//Ejercicio 17 - Divide One Decimal by Another with JavaScript
+
+var quotient1 = 4.4 / 2.0; // Change this line
+
+//Ejercicio 18 - Finding a Remainder in JavaScript
+
+const remainder = 11 % 3;
+
+//Ejercicio 19 - Compound Assignment With Augmented Addition
+
+let a1 = 3;
+let b1 = 17;
+let c1 = 12;
+
+// Only change code below this line
+a1 += 12;
+b1 += 9;
+c1 += 7;
+
+//Ejercicio 20 - Compound Assignment With Augmented Subtraction
+
+let a2 = 11;
+let b2 = 9;
+let c2 = 3;
+
+// Only change code below this line
+a2 -= 6;
+b2 -= 15;
+c2 -= 1;
+
+//Ejercicio 21 - Compound Assignment With Augmented Multiplication
+
+let a3 = 5;
+let b3 = 12;
+let c3 = 4.6;
+
+// Only change code below this line
+a3 *= 5 ;
+b3 *= 3;
+c3 *= 10;
+
+
+
