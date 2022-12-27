79a3979d4672cab83e964bdf8029a7020ef96eba
Lucas Gauna <lks.gauna@gmail.com>
2022-12-27 17:25:42 -0300

    creacion de rutas para peticiones

--- Practicas en Clase/practicaExpressMongo/routes/user.js
+++ Practicas en Clase/practicaExpressMongo/routes/user.js
@@ -1,31 +1,67 @@
 const express = require('express');
 const route = express.Router();
-const userModel = require('../schemas/user');
+const userModel = require('../schemas/user.js');
 
-route.get('/', (req, res) => {
-    res.json({status:200, data:{}})
-});
-
-route.post('/create', (req, res) => {
-    console.log(req.body);
-    
-    const user = new userModel({
-        id: req.body.id,
-        name: req.body.name,
-        lastName: req.body.lastName,
-        age: req.body.age
+route.get("/", (req, res) => {
+    userModel.find({}, (error, data) => {
+      if (error) {
+        res.json({ status: 500, data: error });
+      }
+  
+      res.json({ status: 200, data });
     });
-
-    user.save()
-    .then(document => {
-        res.json({status:200, data:document})
-    })
-    .catch(error => {
-        res.json({status:500, data:error})
-    })
-
-
-    res.json({status:200, data:{}})
-});
+  });
+  
+  route.get("/:id", (req, res) => {
+    userModel.find({ id: req.params.id }, (error, data) => {
+      if (error) {
+        res.json({ status: 500, data: error });
+      }
+  
+      res.json({ status: 200, data });
+    });
+  });
+  
+  route.post("/create", (req, res) => {
+    console.log("El body es: ", req.body);
+  
+    const user = new userModel(req.body);
+  
+    user
+      .save()
+      .then((document) => {
+        res.json({ status: 200, data: document });
+      })
+      .catch((error) => {
+        res.json({ status: 500, data: error });
+      });
+  });
+  
+  route.put("/:id", (req, res) => {
+    userModel.findOneAndUpdate(
+      { id: req.params.id },
+      req.body,
+      {
+        new: true
+      },
+      (error, data) => {
+        if (error) {
+          res.json({ status: 500, data: error });
+        }
+  
+        res.json({ status: 200, data });
+      }
+    );
+  });
+  
+  route.delete("/:id", (req, res) => {
+    userModel.findOneAndDelete({ id: req.params.id }, {}, (error, data) => {
+      if (error) {
+        res.json({ status: 500, data: error });
+      }
+  
+      res.json({ status: 200, data });
+    });
+  });
 
 module.exports = route;
--- Practicas en Clase/practicaExpressMongo/schemas/user.js
+++ Practicas en Clase/practicaExpressMongo/schemas/user.js
@@ -10,4 +10,4 @@ const userSchema = new Schema({
 
 const userModel = mongoose.model('users', userSchema);
 
-mondule.exports = userModel;
+module.exports = userModel;
